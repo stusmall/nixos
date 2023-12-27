bdc19d7535473457beb65dd3a1a668a71463d193
Stu Small <stuart.alan.small@gmail.com>
2023-12-27 08:31:31 -0700

    Switch to nivida closed source driver

--- dell.nix
+++ dell.nix
@@ -1,10 +1,11 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{ config, lib, modulesPath, ... }:
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ./base.nix
-    ./modules/encrypted-dns.nix
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
+      ./base.nix
+      ./modules/encrypted-dns.nix
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
@@ -19,14 +20,16 @@
   networking.hostName = "dell-3551";
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/6b3805a2-8df8-4407-aacc-dd32784075a8";
+    {
+      device = "/dev/disk/by-uuid/6b3805a2-8df8-4407-aacc-dd32784075a8";
       fsType = "ext4";
     };
 
   boot.initrd.luks.devices."luks-1b1be12b-e63c-4077-aae8-f90352cd9d68".device = "/dev/disk/by-uuid/1b1be12b-e63c-4077-aae8-f90352cd9d68";
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/950B-E02A";
+    {
+      device = "/dev/disk/by-uuid/950B-E02A";
       fsType = "vfat";
     };
 
@@ -36,4 +39,44 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
 }
