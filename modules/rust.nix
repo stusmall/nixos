4e78a05f4b4fd402d94e9dff9063debc01899a10
Stu Small <stuart.alan.small@gmail.com>
2024-02-26 08:22:16 -0700

    Add GitHub action for checking formatting

--- modules/rust.nix
+++ modules/rust.nix
@@ -5,30 +5,30 @@
     rustup
   ];
 
-      services.opensnitch.rules = {
-        rule-500-cargo = {
-          name = "Allow cargo to reach crates.io";
-          enabled = true;
-          action = "allow";
-          duration = "always";
-          operator = {
-            type = "list";
-            operand = "list";
-            list = [
-              {
-                type = "regex";
-                sensitive = false;
-                operand = "process.path";
-                data = "^/home/stusmall/.rustup/toolchains/*/bin/cargo$";
-              }
-            {
-              type = "regexp";
-              operand = "dest.host";
-              sensitive = false;
-              data = "^(([a-z0-9|-]+\.)*crates\.io)$";
-            }
-            ];
-          };
-        };
+  services.opensnitch.rules = {
+    rule-500-cargo = {
+      name = "Allow cargo to reach crates.io";
+      enabled = true;
+      action = "allow";
+      duration = "always";
+      operator = {
+        type = "list";
+        operand = "list";
+        list = [
+          {
+            type = "regex";
+            sensitive = false;
+            operand = "process.path";
+            data = "^/home/stusmall/.rustup/toolchains/*/bin/cargo$";
+          }
+          {
+            type = "regexp";
+            operand = "dest.host";
+            sensitive = false;
+            data = "^(([a-z0-9|-]+\.)*crates\.io)$";
+          }
+        ];
       };
+    };
+  };
 }
